9fd6dfb474fac0be8b204a584974d455e5aa04a2
sadman673 <faraz.mahmud07@gmail.com>
2021-02-16 10:40:28 +0600

    MID_LAB_TASK_PHP_05

---
+++ MID_LAB_TASK_PHP_05/task_A/handler.php
@@ -0,0 +1,19 @@
+<?php
+	
+
+	if(isset($_POST['submit'])){
+
+		$name 		=  $_POST['myname'];
+
+		if($name == ""){
+			echo "Null submission...";
+		}else{
+			echo "Success";
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
+
+?>
