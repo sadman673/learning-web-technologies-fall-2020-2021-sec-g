633954f2dab3ae121577c1f75ce638938866667a
sadman673 <faraz.mahmud07@gmail.com>
2021-02-23 09:26:13 +0600

    MID_LAB_TASK__PHP_06

---
+++ MID_LAB_TASK__PHP_06/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>LOGIN</title>
+</head>
+<body>
+	
+                
+                <form action="logincheck.php" method="post">
+                    
+                    <fieldset style="width:320px">
+                        
+                        <legend><b>LOGIN</b></legend>
+                        
+                        User Name :<input type="text" name="uname"><br><br>
+                        Password :<input type="password" name="password">
+                        <hr>
+                        <input type="checkbox" name="remember_me" value=""> <b>Remember Me</b><br><br>
+                        <input type="submit" name="submit" value="Submit"> <a href="forgot_password.php">Forgot Password?</a>
+                        
+                    </fieldset>
+                    
+                </form>
+                
+            
+	
+</body>
+</html>
